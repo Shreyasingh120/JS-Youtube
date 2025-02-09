311d0275b17b7b112d9a3a9019fc69a25d685d9f
Shreyasingh120 <128481198+Shreyasingh120@users.noreply.github.com>
2025-02-09 08:21:19 +0000

    datatypes and variables

---
+++ 01_Basic/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"//treat all js code as newer version
+ // alert(3+3) // error because we are using node js not browser in browser it will run as it is document file
+
+ //tc39 and mdn for explanation
+
+ //number=2 to the power 53
+ //bigInt
+ //string=""
+ //boolean=true/false
+ //null=>standalone value
+ //undefined=variable is declared but value is not assigned to it
+ //symbol=unique
+
+ //non-primitive
+ //object
+
+ console.log(typeof undefined);//undefined
+ console.log(typeof null)//object
---
+++ 01_Basic/variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+var accountEmail = "shreya.singh4494@google.com"
+var accountPassword = "52456"
+accountCity = "Jaipur"
+
+//accountId=2 //not allowed-
+
+accountEmail="sk084@hc.com"
+accountPassword="254852"
+accountCity = "Bengluru"
+
+console.log("accountId")
+console.table([accountEmail,accountPassword,accountCity])
+
+// prefer not to use var
+// because of the issue in block scope and functional scope
