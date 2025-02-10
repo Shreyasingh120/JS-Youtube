29d1492694166283e929a960dba07f6e636415f0
unknown <sk0656968@gmail.com>
2025-02-10 21:35:03 +0530

    conversion of datatypes

---
+++ 01_Basic/conversionOfDatatypes3.js
@@ -0,0 +1,32 @@
+let score=null;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//33=number
+//"33"=number or 33
+//"33abc"="number" or nan
+//true=1
+//null=0
+// undefined=nan
+
+//////Boolean
+
+let isBoolean=1;
+// let converInBoolean=Boolean(isBoolean);
+
+// console.log(converInBoolean)
+
+//1=true, empty or 0=false
+//""=false
+//"hitesh" or string=true
+
+/////string
+
+let isString=1
+let convertInString=String(isString)
+console.log(convertInString)
