42f5e6c034e8ff6118ba736bca925e3ded9d6535
unknown <sk0656968@gmail.com>
2025-02-11 12:44:58 +0530

    comparison and conversion of datatypes

--- 01_Basic/conversionOfDatatypes3.js
+++ 01_Basic/conversionOfDatatypes3.js
@@ -29,4 +29,36 @@ let isBoolean=1;
 
 let isString=1
 let convertInString=String(isString)
-console.log(convertInString)
+// console.log(convertInString)
+
+//***********************OPERATIONS***************** */
+
+let value=1;
+let negValue=-value;
+
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2);
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**3)
+// console.log(2%2)
+
+let str1="hello";
+let str2="shreya"
+
+// console.log(str1+str2)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+"2");
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(true)
+// console.log(+true)
+
+
+// to study prefix and postfix operators
+
